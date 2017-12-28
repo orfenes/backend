98eb4e5b13ad656be70d2701a71c4cd9c585b8d4
Rodrigo Castro <rfocastro@sp.r7.com>
2017-12-28 18:34:16 -0200

    modificando validacao

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -2,7 +2,7 @@ const restful  = require('node-restful');
 const mongoose = restful.mongoose;
 
 const todoSchema = ({
-  description: { type: String, required: true },
+  description: { type: String, required: false },
   done: { type: Boolean, required: true, default: false },
   createdAt: { type: Date, default: Date.now }
 })
